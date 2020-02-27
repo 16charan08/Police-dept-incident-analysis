f62d107e8fddf7586f87a6bfb7927b004026ee78
Ramcharan Reddy.K <ramcharankankanala@gmail.com>
2020-02-26 22:36:28 -0600

    Update test_download.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,1 +1,39 @@
 
+import os
+import pytest
+from project0 import p0
+
+#url = 'http://normanpd.normanok.gov/filebrowser_download/657/2020-02-20%20Daily%20Incident%20Summary.pdf'
+def test_fetchincidents():
+    cwd = os.getcwd()
+    print(cwd)
+    try:
+         assert open('../incidents.pdf', 'rb')
+         print("Incidents files created")
+    except:
+        print("No file exsisted with that name; please try to execute main.py")
+
+
+def test_extractincidents():
+
+    try:
+        a = p0.extractincidents()
+        assert len(a)>1
+        print("Data frame created")
+    except:
+        print("Error Data frame not created")
+
+
+def test_dbcreated():
+    try:
+        database = '../project0/normanpd.db'
+        print(database)
+    except:
+        print("Error Data frame not created")
+
+
+
+
+test_fetchincidents()
+test_extractincidents()
+test_dbcreated()
