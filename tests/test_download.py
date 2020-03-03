69e8f0b3e8dd783c6ead951d7bc425b59b93333c
Ramcharan Reddy.K <ramcharankankanala@gmail.com>
2020-03-03 17:06:52 -0600

    Update test_download.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,38 +1,50 @@
 import os
+import sys
 import pytest
+import sqlite3
+from sqlite3 import Error
 from project0 import p0
+import wget
+#import p0
 
-#url = 'http://normanpd.normanok.gov/filebrowser_download/657/2020-02-20%20Daily%20Incident%20Summary.pdf'
+url = 'http://normanpd.normanok.gov/filebrowser_download/657/2020-02-26%20Daily%20Incident%20Summary.pdf'
+database = 'normanpd.db'
 def test_fetchincidents():
-    cwd = os.getcwd()
-    print(cwd)
-    try:
-         assert open('./incidents.pdf', 'rb')
-         print("Incidents files created")
-    except:
-        print("No file exsisted with that name; please try to execute main.py")
-
+        assert open('./incidents.pdf', 'rb') is not None
 
 def test_extractincidents():
-
-    try:
-        a = p0.extractincidents()
-        assert len(a)>1
-        print("Data frame created")
-    except:
-        print("Error Data frame not created")
-
+    a = p0.extractincidents()
+    assert len(a)>1
 
 def test_dbcreated():
-    try:
-        database = '../project0/normanpd.db'
-        print(database)
-    except:
-        print("Error Data frame not created")
+    dname = p0.createdb()
+    assert dname == database
+
+def test_data_inserted():
+    dname = p0.createdb()
+    a = p0.extractincidents()
+    p0.populatedb(dname,a)
+    sql = sqlite3.connect(database)
+    cursor = sql.cursor()
+    cursor.execute('select * from incidents')
+    assert cursor.fetchall() is not None
+    sql.close()
+
+
+def test_status():
+    dname = p0.createdb()
+    a = p0.extractincidents()
+    p0.populatedb(dname, a)
+    p0.status(dname)
+    sql = sqlite3.connect(database)
+    cursor = sql.cursor()
+    cursor.execute('''select nature,count(nature) from incidents group by nature order by nature''')
+    assert cursor.fetchall() is not None
+    sql.close()
 
 
 
 
-test_fetchincidents()
-test_extractincidents()
-test_dbcreated()
+
+
+
